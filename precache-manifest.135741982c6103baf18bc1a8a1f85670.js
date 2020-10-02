6377d5dda285e249eede3ccda03d7d6ad6e52306
Eli Sultanov <elisultanov@Elis-MacBook-Pro.local>
2020-10-02 15:35:28 -0400

    public files

---
+++ precache-manifest.135741982c6103baf18bc1a8a1f85670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60504ec516d0ae07a54ee5265af708eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3aff4d3480b8bc990de3",
+    "url": "./static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "b2450ed53fd2a83cb9f7",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3aff4d3480b8bc990de3",
+    "url": "./static/js/2.ec2c3ef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ec2c3ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2450ed53fd2a83cb9f7",
+    "url": "./static/js/main.195215a3.chunk.js"
+  },
+  {
+    "revision": "8bbd6c4b0decec96f213",
+    "url": "./static/js/runtime-main.cb3a5fa8.js"
+  }
+]);
